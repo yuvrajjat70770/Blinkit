1eaf249da69f9c48fba5f9f66ca0c652f21b4e09
yuvraj choudhary <118204278+yuvrajjat70770@users.noreply.github.com>
2025-07-31 18:43:38 +0530

    Update index.js
    port set

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(helmet({
     crossOriginResourcePolicy : false
 }))
 
-const PORT = 8080 || process.env.PORT 
+const PORT = process.env.PORT || 8080
 
 app.get("/",(request,response)=>{
     ///server to client
